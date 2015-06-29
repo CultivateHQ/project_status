24e105d5f80c1804fc4e9d04b86ef407c57ab52d
Paul Wilson <paulanthonywilson@gmail.com>
2015-06-29 23:34:29 +0100

    rather silly working out the js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,13 +2,25 @@ import {Socket} from "phoenix"
 
 let socket = new Socket("/ws")
 socket.connect()
-let chan = socket.chan("project_email_recipients:123", {})
-console.log(chan)
-chan.join().receive("ok", chan => {
-  console.log("Success!")
-})
 
 let App = {
+    init: () => {
+        if ($('#new_project_email_recipient').length > 0) App.init_new_email_recipients()
+    },
+    init_new_email_recipients: () => {
+        let chan = socket.chan("project_email_recipients:123", {})
+        chan.join().receive("ok", chan => {
+            console.log("Wat! Success!")
+        })
+
+        $('#new_project_email_recipient').submit(e => {
+            let name = $('#project_email_recipient_name').val()
+            let email = $('#project_email_recipient_email').val()
+            console.log(name, email, "yar!")
+            e.preventDefault()
+        })
+    }
 }
 
 export default App
+App.init()
