bfd46228e8026c6e12bd26799104e7ade3d554a1
Paul Wilson <paulanthonywilson@gmail.com>
2015-07-09 10:34:02 +0200

    ping and shout channel examples removed

--- test/channels/project_email_recipient_channel_test.exs
+++ test/channels/project_email_recipient_channel_test.exs
@@ -63,21 +63,4 @@ defmodule ProjectStatus.ProjectEmailRecipientChannelTest do
       assert_reply ref, :ok, %{email_recipient: %{}} #without assert message is not pushed
     end
   end
-
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-
-
-  test "shout broadcasts to project_email_recipients:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
-  test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from! socket, "broadcast", %{"some" => "data"}
-    assert_push "broadcast", %{"some" => "data"}
-  end
 end
--- web/channels/project_email_recipient_channel.ex
+++ web/channels/project_email_recipient_channel.ex
@@ -40,21 +40,6 @@ defmodule ProjectStatus.ProjectEmailRecipientChannel do
     {:reply, {:ok, %{id: id}}, socket}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (project_email_recipients:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
-    {:noreply, socket}
-  end
-
-  # This is invoked every time a notification is being broadcast
-  # to the client. The default implementation is just to push it
-  # downstream but one could filter or change the event.
-  def handle_out(event, payload, socket) do
-    push socket, event, payload
-    {:noreply, socket}
-  end
-
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
