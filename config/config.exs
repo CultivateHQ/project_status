62fe41e9cb14fc35c09ebe30e5d4f6fa4eb04ff2
Paul Wilson <paulanthonywilson@gmail.com>
2015-08-11 17:37:35 +0100

    Get basic auth credentials from config

--- config/config.exs
+++ config/config.exs
@@ -19,6 +19,8 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :basic_auth, username: 'bob', password: 'unguessable'
+
 import_config "config.secret.exs"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- web/basic_credentials.ex
+++ web/basic_credentials.ex
@@ -1,5 +1,6 @@
 defmodule ProjectStatus.Credentials do
-  def username, do: "user"
-  def password, do: "pwd"
+  import Application, only: [get_env: 2]
+  def username, do: get_env(:basic_auth, :username)
+  def password, do: get_env(:basic_auth, :password)
   def encoded, do: "Basic " <> Base.encode64("#{username}:#{password}")
 end 
