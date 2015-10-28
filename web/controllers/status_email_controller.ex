b7329ffbcc78ef6a440e2eeb48f4e1d957e73d36
Paul Wilson <paulanthonywilson@gmail.com>
2015-10-28 00:44:16 +0000

    quick fix

--- web/controllers/status_email_controller.ex
+++ web/controllers/status_email_controller.ex
@@ -3,7 +3,7 @@ defmodule ProjectStatus.StatusEmailController do
   alias ProjectStatus.ProjectEmails
 
   def show(conn, params) do
-    {:ok, status_email} = ProjectEmails.project_status_email(params["project_id"], params["id"])
+    {:ok, status_email} = ProjectEmails.do_project_status_email(params["project_id"], params["id"])
     render(conn, "show.html", %{status_email: status_email, project_id: params["project_id"]})
   end
 end
