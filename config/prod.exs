56fc8a15e4a9b78f3f975cd638b6186eab784e46
Paul Wilson <paulanthonywilson@gmail.com>
2015-10-29 17:35:59 +0000

    ansibleish

--- config/prod.exs
+++ config/prod.exs
@@ -1,26 +1,26 @@
 use Mix.Config
 config :project_status, ProjectStatus.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [host: "cultiprojectstatus.herokuapp.com"],
+  url: [host: "projectstatus.cultivatehq.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :logger, level: :info
 
 #import_config "prod.secret.exs"
 
-config :project_status, ProjectStatus.Endpoint,
-       secret_key_base: System.get_env("SECRET_KEY_BASE")
+# config :project_status, ProjectStatus.Endpoint,
+#        secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-# Configure your database
-config :project_status, ProjectStatus.Repo,
-       adapter: Ecto.Adapters.Postgres,
-       url: System.get_env("DATABASE_URL"),
-       pool_size: 20
+# # Configure your database
+# config :project_status, ProjectStatus.Repo,
+#        adapter: Ecto.Adapters.Postgres,
+#        url: System.get_env("DATABASE_URL"),
+#        pool_size: 20
 
-config :basic_auth, realm: "Project Status",
-        username: System.get_env("BASIC_AUTH_USER"),
-        password: System.get_env("BASIC_AUTH_PASSWORD")
+# config :basic_auth, realm: "Project Status",
+#         username: System.get_env("BASIC_AUTH_USER"),
+#         password: System.get_env("BASIC_AUTH_PASSWORD")
 
-config :project_status,
-  mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
-  mailgun_key: System.get_env("MAILGUN_KEY")
+# config :project_status,
+#   mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
+#   mailgun_key: System.get_env("MAILGUN_KEY")
