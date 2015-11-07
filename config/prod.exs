6e496582294b2bf9b60b41f4d72e9f391cb4087b
Paul Wilson <paulanthonywilson@gmail.com>
2015-11-07 16:33:46 +0000

    prepare for exrm release

--- config/prod.exs
+++ config/prod.exs
@@ -1,26 +1,9 @@
 use Mix.Config
 config :project_status, ProjectStatus.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [host: "cultiprojectstatus.herokuapp.com"],
+  url: [host: "projectstatus.cultivatehq.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :logger, level: :info
 
-#import_config "prod.secret.exs"
-
-config :project_status, ProjectStatus.Endpoint,
-       secret_key_base: System.get_env("SECRET_KEY_BASE")
-
-# Configure your database
-config :project_status, ProjectStatus.Repo,
-       adapter: Ecto.Adapters.Postgres,
-       url: System.get_env("DATABASE_URL"),
-       pool_size: 20
-
-config :basic_auth, realm: "Project Status",
-        username: System.get_env("BASIC_AUTH_USER"),
-        password: System.get_env("BASIC_AUTH_PASSWORD")
-
-config :project_status,
-  mailgun_domain: System.get_env("MAILGUN_DOMAIN"),
-  mailgun_key: System.get_env("MAILGUN_KEY")
+import_config "prod.secret.exs"
