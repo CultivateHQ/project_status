0eac236671957fe2926928096e58f201d2e94697
Paul Wilson <paulanthonywilson@gmail.com>
2016-02-08 15:29:00 +0000

    move Guardian key out of prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -8,8 +8,3 @@ config :logger, level: :info
 
 import_config "prod.secret.exs"
 
-config :guardian, Guardian,
-  issuer: "ProjectStatus",
-  ttl: { 30, :days },
-  secret_key: "JqTPmxSIJfyBhqOiiLap+5vTq7HEHl0HnVl8b7cdskZD/dajdYSnBYnCVHE29ngX",
-  serializer: ProjectStatus.GuardianSerializer
