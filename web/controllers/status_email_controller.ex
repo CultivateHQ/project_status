21bb77a969e217fa41f6028fd9b6722a531f0a56
Mark Connell <mark@markconnell.co.uk>
2016-03-03 14:21:18 +0000

    add basic header to status email show action

--- web/controllers/status_email_controller.ex
+++ web/controllers/status_email_controller.ex
@@ -1,9 +1,12 @@
 defmodule ProjectStatus.StatusEmailController do
   use ProjectStatus.Web, :controller
   alias ProjectStatus.ProjectEmails
+  alias ProjectStatus.Project
 
   def show(conn, params) do
     {:ok, status_email} = ProjectEmails.do_project_status_email(params["project_id"], params["id"])
-    render(conn, "show.html", %{status_email: status_email, project_id: params["project_id"]})
+
+    project = Repo.get(Project, params["project_id"])
+    render(conn, "show.html", %{status_email: status_email, project: project, project_id: params["project_id"]})
   end
 end
